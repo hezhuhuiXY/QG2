582fd23efd88e232c4d8ec0c8aef0bb6819d5b31
xianchan <hezhuhui@mails.gdut.edu.cn>
2026-04-18 13:19:57 +0800

    Update WebConfig.java

--- backend/src/main/java/com/qg/lostfound/config/WebConfig.java
+++ backend/src/main/java/com/qg/lostfound/config/WebConfig.java
@@ -12,11 +12,14 @@ import java.nio.file.Paths;
 public class WebConfig implements WebMvcConfigurer {
 
     private final UploadProperties uploadProperties;
-
+/**
+     * 配置静态资源访问映射
+     * 作用：把 服务器本地磁盘路径 映射为 浏览器可访问的 URL
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String location = Paths.get(uploadProperties.getDir()).toUri().toString();
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations(location);
+                .addResourceLocations(location);// 映射到服务器本地磁盘路径
     }
 }
